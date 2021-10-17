dcb6ce979d187c4685d5ec9282de5245bf398f16
sanix-darker <s4nixd@gmail.com>
2021-10-18 00:25:57 +0200

    feat(opencollective): notify telegram for opencollective stats

---
+++ .github/workflows/opencollective_contributions/get_opencollective_info.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+set -e
+
+# We make the request and save response in json file
+echo $(curl -s 'https://opencollective.com/osscameroon.json') > opencollective_info.json
+
+# We parse and extract datas...
+balance=$(($(cat opencollective_info.json | jq -r '.balance')/10))
+yearlyIncome=$(($(cat opencollective_info.json | jq -r '.yearlyIncome')/10))
+backersCount=$(cat opencollective_info.json | jq -r '.backersCount')
+
+
+# We build our message
+echo "\`\`\`" #for telegram format
+
+current_date=$(date)
+echo "$current_date"
+echo ""
+echo "OpenCollective report"
+echo ""
+echo ""
+echo "🧐 Hello there"
+echo ""
+echo "This is some statistics for our opencollective contributions (yeah, i mean... monney/'nkap' inside Oss Cameroon):"
+echo ""
+echo "----------------------------------------------"
+echo ""
+echo "[💶] TODAY’S BALANCE : ${balance}€"
+echo "[💸] ESTIMATED ANNUAL BUDGET : ${yearlyIncome}€"
+echo "[😎] BAKERS/CONTRIBUTORS : ${backersCount}"
+echo ""
+echo "----------------------------------------------"
+echo ""
+echo "Huges Thanks for the donations, the trust and the support 🥳🙇🙏 !!!"
+echo ""
+echo "If you want to contribute too, feel free to donate here : https://opencollective.com/osscameroon !"
+echo "There is no 'small monney', each donations count a lot😉!"
+echo ""
+echo "Full details about transactions, expenses... available here : https://opencollective.com/osscameroon/transactions"
+
+echo "\`\`\`" #for telegram format
