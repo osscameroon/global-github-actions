856abdde5f785326ed55fe09086ce2c8feef0d63
elhmn <nleme@live.fr>
2021-12-24 15:30:14 +0100

    feat: read messages from res/messages.yaml

--- .github/workflows/social_media_message_scheduler/main.py
+++ .github/workflows/social_media_message_scheduler/main.py
@@ -1,3 +1,41 @@
+import yaml
+from twitter import twitter_publisher
+from telegram import telegram_group_publisher, telegram_channel_publisher
+
+MESSAGE_FILE = "./res/messages.yaml"
+
+func_publishers = {
+    "twitter": twitter_publisher,
+    "telegram_group": telegram_group_publisher,
+    "telegram_channel": telegram_channel_publisher,
+}
+
+
+def get_messages():
+    with open(MESSAGE_FILE, "r") as file:
+        try:
+            return yaml.safe_load(file)
+        except yaml.YAMLError as err:
+            print(err)
+
+
+def publish_messages(messages):
+    if messages is None:
+        print("Error: No messages found!")
+
+    for message in messages:
+        if "targets" in message:
+            # make sure that we uniq element per target
+            targets = set(message["targets"])
+            print(targets)
+
+            for target in targets:
+                if target in func_publishers:
+                    func_publishers[target](message)
+            print("\n")
+
 
 if __name__ == "__main__":
-    print("je suis con")
+    messages = get_messages()
+    publish_messages(messages)
+#     print(messages)
---
+++ .github/workflows/social_media_message_scheduler/twitter.py
@@ -0,0 +1,4 @@
+def twitter_publisher(message):
+    if message is None:
+        print("Error message not found")
+    print("Twitter publisher: \n", message)
