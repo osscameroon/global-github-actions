480cca644c04bb0380dd2198a3c03f720c95d626
sanix-darker <s4nixd@gmail.com>
2021-12-30 23:35:36 +0100

    fix: added some fixes and shortened the quiz combo message

--- .github/workflows/report_quiz/report_quiz.sh
+++ .github/workflows/report_quiz/report_quiz.sh
@@ -7,6 +7,11 @@ CHAT_ID=$1
 TELEGRAM_BOT_TOKEN=$2
 QUIZAPI_KEY=$3
 
+# To escapes some stupid quotes
+escp(){
+    echo -e "$(printf '%q' $1)"
+}
+
 # to fetch programming questions
 get_quiz_questions_answers() {
     curl https://quizapi.io/api/v1/questions -G -d apiKey=$QUIZAPI_KEY -d limit=1 -d difficulty=easy 2>/dev/null | \
@@ -16,7 +21,7 @@ get_quiz_questions_answers() {
 # A curl to send message in a chat
 send_message(){
     curl -s \
-    -d "{\"chat_id\": $1, \"text\": \"${2:q}\"}" \
+        -d "{\"chat_id\": $1, \"text\": \"${2}\"}" \
     -H "Content-Type: application/json" -X POST https://api.telegram.org/bot$TELEGRAM_BOT_TOKEN/sendMessage
 }
 
@@ -24,16 +29,16 @@ send_message(){
 send_poll(){
     curl -s \
     -d "{\"chat_id\": $1, \
-            \"question\": \"😏 Can you guess the good answer(s) ?\n🤪 Don't worry it's anonymous !\n\n\",\
-            \"options\": [\"🤔 Option A\",\"😯 Option B\",\"🤧 Option C\",\"🥴 Option D\",\"😑 Option E\"]}" \
+            \"question\": \"😏 Can you guess the good answer(s) ?\n🤪 Don't worry it's anonymous !\",\
+            \"options\": [\"🤔 Option A\",\"😯 Option B\",\"🤧 Option C\",\"🥴 Option D\"]}" \
     -H "Content-Type: application/json" -X POST https://api.telegram.org/bot$TELEGRAM_BOT_TOKEN/sendPoll
 }
 
 ret=$(get_quiz_questions_answers)
 echo $ret | jq -r '[.question, .A, .B, .C, .D, .E] | @tsv' | \
 while IFS=$'\t' read -r question A B C D E; do
-    send_message $CHAT_ID "👨🏾‍💻 Quiz time !?\n\nQ: ${question:q} \n#oss_quiz_time"
-    send_message $CHAT_ID "\nA: ${A:q}\nB: ${B:q}\nC: ${C:q}\nD: ${D:q}\nE: ${E:q}\n\n#oss_quiz_time"
+    msg="👨🏾💻 Quiz ?\n${question} \n\nA $(escp ${A})\nB $(escp ${B})\nC $(escp ${C})\nD $(escp ${D})"
+
+    send_message $CHAT_ID "${msg}"
     send_poll $CHAT_ID
-    send_message $CHAT_ID "😇 It is allowed to discuss about it in the chat and exchange with others on what you think about it !\n\n#oss_quiz_time"
 done
