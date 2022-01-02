822476aa7d0f5d6ed1e4e25509674d223c371341
elhmn <nleme@live.fr>
2022-01-02 17:07:22 +0100

    feat: rollback code abstration

--- .github/workflows/social_media_message_scheduler/twitter.py
+++ .github/workflows/social_media_message_scheduler/twitter.py
@@ -12,22 +12,22 @@ api = tweepy.API(auth)
 
 
 def post_tweet(text):
-    if text:
-        print("Twitter publisher: \n", text)
-        print("Lenght: ", len(text))
-        api.update_status(text)
+    print("Twitter publisher: \n", text)
+    print("Lenght: ", len(text))
+    api.update_status(text)
 
 
 def twitter_publisher(message):
     if message is None:
         raise Exception("Error message not found")
 
-    text = None
     if "message" in message:
         text = message["message"]
+        post_tweet(text)
     else:
         if "message-en" in message:
             text = message["message-en"]
+            post_tweet(text)
         if "message-fr" in message:
             text = message["message-fr"]
-    post_tweet(text)
+            post_tweet(text)
