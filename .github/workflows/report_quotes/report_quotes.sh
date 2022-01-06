7a072820757cfeb789deebcf49ed929a71e94347
darker <s4nixd@gmail.com>
2022-01-06 18:22:55 +0100

    feat: report quotes (#45)
    * feat: added a github action for sending programming quotes

---
+++ .github/workflows/report_quotes/report_quotes.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+set -e
+
+
+get_quote(){
+    res=$(curl -s -L https://programming-quotes-api.herokuapp.com/quotes/random | jq '{a: .author, q: .en}')
+    echo $res
+}
+
+result=$(get_quote)
+author=$(echo $result | jq '.a')
+quote=$(echo $result | jq '.q')
+
+echo ""
+echo "📖 Random Programming Quote !"
+echo ""
+echo " \`\`\`"
+echo "$quote"
+echo " \`\`\`"
+echo "From $author"
+echo ""
+
