5d5bc711338676651e44625606c074528becf452
darker <s4nixd@gmail.com>
2022-01-09 11:45:51 +0100

    fix: yet another weird fix to prevent having in the poll options with spaces (#52)

--- .github/workflows/report_quiz/report_quiz.sh
+++ .github/workflows/report_quiz/report_quiz.sh
@@ -2,16 +2,29 @@
 
 set -e
 
-
+# incomming parameters
 CHAT_ID=$1
 TELEGRAM_BOT_TOKEN=$2
 QUIZAPI_KEY=$3
 
+# Count spaces from an incomming string
+# And exit if count spaces greather than 0
+exit_if_spaces(){
+    c="${1//[^ ]}"
+    if [[ ${#c} -gt 0 ]]
+    then
+        exit 0
+    fi
+}
+
 # To escapes some stupid quotes
 escp(){
+    # exit if there is spaces in the options
+    exit_if_spaces
+
     if [[ ${#2} -gt 1 ]]
     then
-        echo "$1 $(printf '%q' $2)\""
+        echo -e "$1 $(printf '%q' $2)\""
     fi
 }
 
@@ -39,20 +52,25 @@ send_poll(){
     curl -s -d "$payload" -H "Content-Type: application/json" -X POST https://api.telegram.org/bot$TELEGRAM_BOT_TOKEN/sendPoll
 }
 
-ret=$(get_quiz_questions_answers)
-echo $ret | jq -r '[.question, .A, .B, .C, .D] | @tsv' | \
-while IFS=$'\t' read -r question A B C D; do
-    msg="👨🏾💻 Quiz Time !?\n${question}"
-    options=$(escp "\"A:" $A)$(escp ",\"B:" $B)$(escp ",\"C:" $C)$(escp ",\"D:" $D)
+# The main function to be executed
+main(){
+    ret=$(get_quiz_questions_answers)
+    echo $ret | jq -r '[.question, .A, .B, .C, .D] | @tsv' | \
+    while IFS=$'\t' read -r question A B C D; do
+        msg="👨🏾💻 Quiz Time !?\n${question}"
+        options=$(escp "\"" $A)$(escp ",\"" $B)$(escp ",\"" $C)$(escp ",\"" $D)
 
-    echo "msg: $msg"
-    echo "----------------------------------------------"
-    echo "options: $options"
-    echo "----------------------------------------------"
-    # Let's remove the first and last trailing commas
-    options=${options#","}
-    options=${options%","}
+        echo "msg: $msg"
+        echo "----------------------------------------------"
+        echo "options: $options"
+        echo "----------------------------------------------"
+        # Let's remove the first and last trailing commas
+        options=${options#","}
+        options=${options%","}
 
-    send_message $CHAT_ID "$msg"
-    send_poll $CHAT_ID "$options"
-done
+        send_message $CHAT_ID "$msg"
+        send_poll $CHAT_ID "$options"
+    done
+}
+
+main
