e97e54b091cd5c3d1d5b8954b45bc482bdf7bf1b
Fomegne <fomegnemeudje@outlook.com>
2022-02-25 23:54:06 +0100

    fix: all the quiz answers was not take in account

--- .github/workflows/report_quiz/report_quiz.sh
+++ .github/workflows/report_quiz/report_quiz.sh
@@ -17,7 +17,7 @@ escp(){
 # to fetch programming questions
 get_quiz_questions_answers() {
     curl https://quizapi.io/api/v1/questions -G -d apiKey=$QUIZAPI_KEY -d limit=1 -d difficulty=easy 2>/dev/null | \
-    jq '.[] | {question: .question, A: .answers.answer_a, B: .answers.answer_b, C: .answers.answer_c, D: .answers.answer_d, E: .answers.answer_e}'
+    jq '.[] | {question: .question, A: .answers.answer_a, B: .answers.answer_b, C: .answers.answer_c, D: .answers.answer_d, E: .answers.answer_e, F: .answers.answer_f}'
 }
 
 # A curl to send message in a chat
@@ -41,12 +41,12 @@ send_poll(){
 # Propose a quiz
 propose_quiz(){
     ret=$(get_quiz_questions_answers)
-    echo $ret | jq -r '[.question, .A, .B, .C, .D] | @tsv' | \
+    echo $ret | jq -r '[.question, .A, .B, .C, .D, .E, .F] | @tsv' | \
     while IFS=$'\t' read -r question A B C D; do
         msg="👨🏾💻 Quiz Time !?\n${question}"
         # We add the answer if not null
         # We suppose that at this point we will have at least 2 options valid
-        options=$([ -z "$A" ] || echo $(escp "$A"))$([ -z "$B" ] || echo ,$(escp "$B"))$([ -z "$C" ] || echo ,$(escp "$C"))$([ -z "$D" ] || echo ,$(escp "$D"))
+        options=$([ -z "$A" ] || echo $(escp "$A"))$([ -z "$B" ] || echo ,$(escp "$B"))$([ -z "$C" ] || echo ,$(escp "$C"))$([ -z "$D" ] || echo ,$(escp "$D"))$([ -z "$E" ] || echo ,$(escp "$E"))$([ -z "$F" ] || echo ,$(escp "$F"))
         echo $options
         
         echo "msg: $msg"
@@ -55,7 +55,7 @@ propose_quiz(){
         echo "----------------------------------------------"
 
         # We verify if the options don't exceed the max length
-        if [ ${#A} -gt $MAX_LENGTH ] || [ ${#B} -gt $MAX_LENGTH ] || [ ${#C} -gt $MAX_LENGTH ] || [ ${#D} -gt $MAX_LENGTH ]; then
+        if [ ${#A} -gt $MAX_LENGTH ] || [ ${#B} -gt $MAX_LENGTH ] || [ ${#C} -gt $MAX_LENGTH ] || [ ${#D} -gt $MAX_LENGTH ] || [ ${#E} -gt $MAX_LENGTH ] || [ ${#F} -gt $MAX_LENGTH ]; then
             echo "[skipped] one option exceed the max max_length $MAX_LENGTH"
             exit 1
         else
