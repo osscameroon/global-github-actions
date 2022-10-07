45938429f5b2950b67d2e1ba1c00607ba7890cc6
sanix-darker <s4nixd@gmail.com>
2022-10-07 23:05:48 +0200

    feat: setted fetcher and main function for the stdout

--- .github/workflows/report_news/main.py
+++ .github/workflows/report_news/main.py
@@ -1,17 +1,37 @@
+import json
 import requests
 import xml.etree.ElementTree as ET
 
 TECH_GRIOT = 'https://techgriot.co/feed'
 
-r = requests.get(TECH_GRIOT)
-xml_raw = ET.fromstring(r.text)
 
-final_string = ''
-for x in xml_raw.findall('.//item'):
-    title = x.find('./title').text
-    link = x.find('./link').text
-    description = x.find('./description').text
-    # author = x.find('./comments/following-sibling').text
-    # author = x.find("./dc\:creator").text
-    pub_date = x.find('./pubDate').text
-    # post_id = x.find('./post-id').text
+def fetch_articles() -> list:
+    """
+    This function is responsible for fetching and
+    parsing datas from techgrio
+    """
+    r = requests.get(TECH_GRIOT)
+    xml_raw = ET.fromstring(r.text)
+
+    articles = []
+    for x in xml_raw.findall('.//item'):
+        title = x.find('./title').text
+        link = x.find('./link').text
+        description = x.find('./description').text
+        # author = x.find('./comments/following-sibling').text
+        # author = x.find("./dc\:creator").text
+        pub_date = x.find('./pubDate').text
+        # post_id = x.find('./post&minus;id').text
+
+        articles.append({
+            'title': title,
+            'link': link,
+            'description': description,
+            'pub_date': pub_date,
+        })
+
+    return articles
+
+
+if __name__ == '__main__':
+    print(json.dumps(fetch_articles()))
