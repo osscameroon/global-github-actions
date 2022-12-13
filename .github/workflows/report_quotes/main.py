4676ddcb52d3bcb479560eeb9ac899fb1e981840
sanix-darker <s4nixd@gmail.com>
2022-12-13 11:41:04 +0100

    fix: small cleaning

--- .github/workflows/report_quotes/main.py
+++ .github/workflows/report_quotes/main.py
@@ -27,8 +27,8 @@ def render_quote(quote: dict) -> None:
     """ We just render on stdout the quote"""
     validate_quote(quote)
 
-    print('\n📖 Random Programming Quote \\!')
-    print(f'"{quote.get("en")}"\n')
+    print('\n📖 Random Programming Quote !')
+    print(f'\n"{quote.get("en")}"\n')
     print(f'From {quote.get("author")}\n')
 
 
